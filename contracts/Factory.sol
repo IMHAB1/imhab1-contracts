15817c803f232b63af30043b59d658c6baaeff3d
Carl Park <dkdkajej@gmail.com>
2023-02-17 12:50:26 +0900

    add ethers type bindigs

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -4,14 +4,6 @@ pragma solidity ^0.8.13;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// import {SuperfluidFrameworkDeployer} from "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol";
-// import {TestGovernance} from "@superfluid-finance/ethereum-contracts/contracts/utils/TestGovernance.sol";
-// import {Superfluid} from "@superfluid-finance/ethereum-contracts/contracts/superfluid/Superfluid.sol";
-// import {ConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/agreements/ConstantFlowAgreementV1.sol";
-// import {InstantDistributionAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/agreements/InstantDistributionAgreementV1.sol";
-// import {IDAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/IDAv1Library.sol";
-// import {SuperTokenFactory} from "@superfluid-finance/ethereum-contracts/contracts/superfluid/SuperTokenFactory.sol";
-
 import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 
 import {ISuperTokenFactory} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperTokenFactory.sol";
@@ -20,14 +12,10 @@ import {SuperTokenV1Library} from "@superfluid-finance/ethereum-contracts/contra
 
 import {CourseManager} from "./CourseManager.sol";
 import {IBT} from "./IBT.sol";
-import {IBTx} from "./IBTx.sol";
 
 // don't care gas optimization due to L2.
 contract Factory is Ownable {
     using SuperTokenV1Library for ISuperToken;
-
-    // ISuperfluid public host;
-    // IConstantFlowAgreementV1 public cfa;
 
     IBT public ibt;
     ISuperToken public ibtx;
@@ -35,24 +23,7 @@ contract Factory is Ownable {
 
     ISuperTokenFactory private superTokenFactory;
 
-    // struct Framework {
-    //     TestGovernance governance;
-    //     Superfluid host;
-    //     ConstantFlowAgreementV1 cfa;
-    //     InstantDistributionAgreementV1 ida;
-    //     IDAv1Library.InitData idaLib;
-    //     SuperTokenFactory superTokenFactory;
-    // }
-    // SuperfluidFrameworkDeployer.Framework sf;
-    // SuperfluidFrameworkDeployer sfd;
-
     constructor(ISuperTokenFactory _superTokenFactory) {
-        // sfd = new SuperfluidFrameworkDeployer();
-        // sf = sfd.getFramework();
-
-        // host = sf.host;
-        // cfa = sf.cfa;
-
         superTokenFactory = _superTokenFactory;
         deployContracts();
     }
@@ -61,29 +32,6 @@ contract Factory is Ownable {
         ibt = _deployIBT();
         ibtx = _deployIBTx();
         courseManager = _deployCourseManager();
-
-        ibtx.setFlowPermissions(
-            address(courseManager),
-            true,
-            true,
-            true,
-            type(int96).max
-        );
-        ibtx.setMaxFlowPermissions(address(courseManager));
-        // ibtx.updateFlowOperatorPermissions()
-
-        // (
-        //     ISuperfluid host,
-        //     IConstantFlowAgreementV1 cfa
-        // ) = _getAndCacheHostAndCFA(ibtx);
-
-        // cfa.updateFlowOperatorPermissions(
-        //     ibtx,
-        //     address(courseManager),
-        //     7,
-        //     type(int96).max,
-        //     ""
-        // );
     }
 
     function _deployIBT() internal returns (IBT _ibt) {
@@ -102,66 +50,13 @@ contract Factory is Ownable {
         _ibtx = superTokenFactory.createERC20Wrapper(
             IERC20(ibt),
             18,
-            // ISuperTokenFactory.Upgradability.SEMI_UPGRADABLE,
             ISuperTokenFactory.Upgradability.NON_UPGRADABLE,
             "Super IMHAB1 Token",
             "IBTx"
         );
-
-        // (, _ibtx) = sfd.deployWrapperSuperToken(
-        //     "Super IMHAB1 Token",
-        //     "IBTx",
-        //     18,
-        //     1e25
-        // );
     }
 
     function _deployCourseManager() internal returns (CourseManager _cm) {
         _cm = new CourseManager(ibtx);
-    }
-
-    // keccak256("org.superfluid-finance.apps.SuperTokenLibrary.v1.host")
-    bytes32 private constant _HOST_SLOT =
-        0x65599bf746e17a00ea62e3610586992d88101b78eec3cf380706621fb97ea837;
-    // keccak256("org.superfluid-finance.apps.SuperTokenLibrary.v1.cfa")
-    bytes32 private constant _CFA_SLOT =
-        0xb969d79d88acd02d04ed7ee7d43b949e7daf093d363abcfbbc43dfdfd1ce969a;
-    // keccak256("org.superfluid-finance.apps.SuperTokenLibrary.v1.ida");
-    bytes32 private constant _IDA_SLOT =
-        0xa832ee1924ea960211af2df07d65d166232018f613ac6708043cd8f8773eddeb;
-
-    // gets the host and cfa addrs for the token and caches it in storage for gas efficiency
-    // to be used in state changing methods
-    function _getAndCacheHostAndCFA(
-        ISuperToken token
-    ) private returns (ISuperfluid host, IConstantFlowAgreementV1 cfa) {
-        // check if already in contract storage...
-        assembly {
-            // solium-disable-line
-            host := sload(_HOST_SLOT)
-            cfa := sload(_CFA_SLOT)
-        }
-        if (address(cfa) == address(0)) {
-            // framework contract addrs not yet cached, retrieving now...
-            if (address(host) == address(0)) {
-                host = ISuperfluid(token.getHost());
-            }
-            cfa = IConstantFlowAgreementV1(
-                address(
-                    ISuperfluid(host).getAgreementClass(
-                        //keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")
-                        0xa9214cc96615e0085d3bb077758db69497dc2dce3b2b1e97bc93c3d18d83efd3
-                    )
-                )
-            );
-            // now that we got them and are in a transaction context, persist in storage
-            assembly {
-                // solium-disable-line
-                sstore(_HOST_SLOT, host)
-                sstore(_CFA_SLOT, cfa)
-            }
-        }
-        assert(address(host) != address(0));
-        assert(address(cfa) != address(0));
     }
 }
--- contracts/IBTx.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-import {ERC20PresetMinterPauser} from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
-
-/// @dev IBTx SuperToken
-contract IBTx is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("IMHAB1 Token", "IBT") {}
-}
